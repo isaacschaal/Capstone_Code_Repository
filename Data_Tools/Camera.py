db38d080f585e177c82fc98e62a485056181b09e
Isaac Schaal <isaac.h.schaal@gmail.com>
2020-01-27 17:49:37 +0800

    Updated Camera.py

--- Data_Tools/Camera.py
+++ Data_Tools/Camera.py
@@ -2,6 +2,12 @@ import numpy as np
 import cv2
 import time
 import os
+import re
+
+file_list = os.listdir(path='./selfies')
+num_list = [re.findall(r'\d+', string) for string in file_list]
+max_dig = max(num_list)
+new_start = int(max_dig[0]) + 1
 
 # give me time to switch desktops to
 # an empty with just the background image
@@ -15,17 +21,17 @@ cap = cv2.VideoCapture(0)
 # pictures at different angles
 for i in range(10):
     # pause so that I can adjust the angle
-    time.sleep(4)
+    time.sleep(3)
 
     # get a still from the camera
     _, frame = cap.read()
-    
+
     print (".")
     # play audio to know when to switch
     # the angle
     os.system( "say p" )
     # save the photo
-    cv2.imwrite('selfies/frame-'+str(i)+'.png',frame)
+    cv2.imwrite('selfies/frame-'+str(new_start+i)+'.png',frame)
 
 
 
