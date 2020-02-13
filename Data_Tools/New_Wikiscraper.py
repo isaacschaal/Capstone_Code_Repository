56d080d00944c401bb4025a1704f73736d064524
Isaac Schaal <isaac.h.schaal@gmail.com>
2020-02-13 11:40:06 +0800

    scraper update

--- Data_Tools/New_Wikiscraper.py
+++ Data_Tools/New_Wikiscraper.py
@@ -19,8 +19,6 @@ with open('artist_list.csv') as csv_file:
     for line in csv_reader:
         artist_list.append(line[0])
 
-print (artist_list)
-
 output_dir = "artists_scraped"
 
 num_downloaded = 0
@@ -53,23 +51,11 @@ def get_painting_list_by_artist(artist_name):
     except Exception as e:
         print('failed to scrape %s'%artist_name, e)
 
-def get_painting_list(count, typep, searchword):
-    try:
-        time.sleep(3.0*random.random())  # random sleep to decrease concurrence of requests
-        url = "https://www.wikiart.org/en/paintings-by-%s/%s/%d"%(typep, searchword, count)
-        soup = BeautifulSoup(urllib.request.urlopen(url), "lxml")
-        regex = r'https?://uploads[0-9]+[^/\s]+/\S+\.jpg'
-        url_list = re.findall(regex, str(soup.html()))
-        count += len(url_list)
-        return url_list
-    except Exception as e:
-        print('failed to scrape %s'%url, e)
-
 def downloader(link, output_dir, failed_urls):
     global num_downloaded, num_images
     item, file = link
     filepath = file.split('/')
-    savepath = '%s/%s' % (output_dir, filepath[-1])
+    savepath = '%s/%s-%s' % (output_dir, filepath[-2],filepath[-1])
     try:
         time.sleep(0.2)  # try not to get a 403
         urllib.request.urlretrieve(file, savepath)
@@ -84,7 +70,7 @@ def downloader(link, output_dir, failed_urls):
 def main(artist_list, output_dir):
     start = time.time()
     global num_images
-    print('gathering links to images... this may take a few minutes')
+    print('gathering links to images...')
     # create a threadpool with one less than the total number of CPUs of the machine
     # this allows fast multiprocessing w/out overloading the machine
     threadpool = Pool(multiprocessing.cpu_count()-1)
@@ -94,6 +80,10 @@ def main(artist_list, output_dir):
     # but typep and searchword are repeated
     wikiart_pages = threadpool.imap(get_painting_list_by_artist, zip(artist_list))
 
+    # close and join the threadpool as per recommended usage
+    threadpool.close()
+    threadpool.join()
+
     # convert the wikiart iterator into
     pages = [page for page in wikiart_pages if page ]
     items = [item for sublist in pages for item in sublist]
@@ -102,38 +92,70 @@ def main(artist_list, output_dir):
     if not os.path.isdir('%s/'%(output_dir)):
         os.mkdir('%s/'%(output_dir))
 
+    threadpool = Pool(multiprocessing.cpu_count()-1)
+
     failed_urls = []
     print('attempting to download %d images'%num_images)
     threadpool.starmap(downloader, zip(enumerate(items), itertools.repeat(output_dir), itertools.repeat(failed_urls)))
 
-    print ("attempting to download the failed URLs")
-    failed_urls_v2 = []
+    # close and join the threadpool as per recommended usage
+    threadpool.close()
+    threadpool.join()
+
+    print ("Took %d seconds to complete the first run."%(time.time()-start))
+    print ("Attempting to gather the failed URLs")
+
     # Fix failed urls
     corrected_urls = []
     for i,url in enumerate(failed_urls):
+        #show progress
         if i %100 == 0:
             print (str(i)+" out of " +str(len(failed_urls)))
+        # extract the correct part of the url
         url = url[1]
         extracted = re.findall(r'/images/(.*?).jpg', url)
         full_url = "https://www.wikiart.org/en/" + extracted[0]
 
+        # get the soup from the page
         soup = BeautifulSoup(urllib.request.urlopen(full_url), "lxml")
+        #try jpg
         regex = r'<meta content="https://uploads[0-9].wikiart.org(.*?).jpg'
         corrected_link = re.search(regex, str(soup.html()))
+
+        success = False
         if corrected_link:
             full_correct_url = corrected_link.group(0)[15:]
+            corrected_urls.append(full_correct_url)
+            success = True
         else:
+            #try other file formats
+            reg_list = [r'<meta content="https://uploads[0-9].wikiart.org(.*?).png',
+                        r'<meta content="https://uploads[0-9].wikiart.org(.*?).jpeg',
+                        r'<meta content="https://uploads[0-9].wikiart.org(.*?).Jpeg',
+                        r'<meta content="https://uploads[0-9].wikiart.org(.*?).JPG',
+                        r'<meta content="https://uploads[0-9].wikiart.org(.*?).PNG',]
+            for regex in reg_list:
+                if success == False:
+                    corrected_link = re.search(regex, str(soup.html()))
+                    if corrected_link:
+                        full_correct_url = corrected_link.group(0)[15:]
+                        corrected_urls.append(full_correct_url)
+                        success = True
+        if success == False:
             print("fail: "+full_url)
-            #### TRY THE OTHER ONES (.jpeg and .png)
-        corrected_urls.append(full_correct_url)
 
+    print ("Downloading corrected URLs")
+    threadpool = Pool(multiprocessing.cpu_count()-1)
+
+    failed_urls_v2 = []
     threadpool.starmap(downloader, zip(enumerate(corrected_urls), itertools.repeat(output_dir), itertools.repeat(failed_urls_v2)))
 
     # close and join the threadpool as per recommended usage
     threadpool.close()
     threadpool.join()
-    print ("Took %d seconds to complete"%(time.time()-start))
+
+    print ("Took %d seconds to complete."%(time.time()-start))
 
 
-# if __name__ == '__main__':
-#     main(artist_list, output_dir)
+if __name__ == '__main__':
+    main(artist_list, output_dir)
