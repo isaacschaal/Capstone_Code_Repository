a4213809157219b6a989671996a02647fad6ba4a
Isaac Schaal <isaac.h.schaal@gmail.com>
2020-02-24 13:04:25 +0800

    added twitter functionality, along with test images'

---
+++ Autonomous_Artist/create_db.py
@@ -0,0 +1,24 @@
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker,scoped_session
+
+
+engine = db.create_engine('sqlite:///DB.sqlite')
+
+Session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+
+class Artwork(Base):
+    __tablename__ = 'artwork'
+    id = Column(Integer, primary_key=True)
+    week = Column(Integer)
+    name = Column(String)
+    path = Column(String)
+    twitter_id_str = Column(String)
+    winner = Column( Boolean)
+    hosting_url = Column(String)
+    auction_link = Column(String)
+
+Base.metadata.create_all(engine)
---
+++ Autonomous_Artist/main_monday.py
@@ -0,0 +1,31 @@
+import argparse
+import subprocess
+
+
+# Parse Args
+parser = argparse.ArgumentParser()
+parser.add_argument("--week", type=str, help="The number of the week")
+
+def main(week):
+    # On sunday should make the images? So they are ready? Or monday morning?
+    # TO DO, generate images and add to folder
+
+    # 1: Add new images to DB
+    commands = ['python3', 'add_new_imgs_to_DB_monday.py','--week',str(week)]
+    process = subprocess.run(commands,
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    #process
+
+    # 2: Tweet new images
+    commands = ['python3', 'tweet_new_imgs_monday.py','--week',str(week)]
+    process = subprocess.run(commands,
+                            stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    #process
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    week = args.week
+    main(week)
